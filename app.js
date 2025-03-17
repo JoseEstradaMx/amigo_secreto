6c49af7ba5da820ca3b542443bdd25cd1f57abd7
JoseEstradaMx <86747497+JoseEstradaMx@users.noreply.github.com>
2025-03-17 14:21:41 -0600

    Add files via upload
    Se sube proyecto amigo secreto

---
+++ app.js
@@ -0,0 +1,38 @@
+let listaAmigos = [];
+let elementoListaAmigos = document.querySelector('#listaAmigos');
+let elementoAmigoSorteado = document.querySelector('#resultado');
+let amigosRetorno="";
+let amigoPosicionLista=0;
+
+function agregarAmigo(){
+    let amigoIntroducido = document.getElementById('amigo').value.trim();
+    if(amigoIntroducido != ""){
+        listaAmigos.push(amigoIntroducido);
+        amigosRetorno+="<li>"+listaAmigos[amigoPosicionLista]+"</li>";
+        amigoPosicionLista++;
+        elementoListaAmigos.innerHTML=amigosRetorno;
+        limpiarCajaAmigo();
+    }else{
+        alert('Introduce un nombre valido por favor');
+    }
+    return;
+}
+
+function sortearAmigo(){
+    if(listaAmigos.length==0){
+        alert('No existen amigos para sortear');
+    }else{
+        let amigoSorteado = Math.floor((Math.random()*listaAmigos.length));
+        resultado.innerHTML = `<li>El amigo secreto sorteado es: ${listaAmigos[amigoSorteado]}</li>`;
+        elementoListaAmigos.innerHTML="<li></li>";
+        amigosRetorno="";
+        amigoPosicionLista=0;
+        listaAmigos=[];
+    }
+    return;
+}
+
+function limpiarCajaAmigo(){
+    document.querySelector('#amigo').value='';
+    return;
+}
